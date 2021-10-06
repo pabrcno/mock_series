794da7851f11c79fb59e1d613a6879912ed4bad0
pabrcno <pabrcn@gmail.com>
2021-10-06 13:39:43 -0300

    domain started

---
+++ lib/domain/core/value_objects.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/foundation.dart';
+import 'package:dartz/dartz.dart';
+import 'package:mock_series/domain/core/errors.dart';
+import 'package:mock_series/domain/core/failures.dart';
+import 'package:uuid/uuid.dart';
+
+@immutable
+abstract class ValueObject<T> {
+  const ValueObject();
+  Either<ValueFailure<T>, T> get value;
+
+  T getOrCrash() {
+    return value.fold((f) => throw UnexpectedValueError(f), id);
+  }
+
+  bool isValid() => value.isRight();
+  @override
+  // ignore: avoid_renaming_method_parameters
+  bool operator ==(Object o) {
+    if (identical(this, o)) return true;
+    return o is ValueObject<T> && o.value == value;
+  }
+
+  @override
+  int get hashCode => value.hashCode;
+
+  @override
+  String toString() => 'Value($value)';
+}
+
+class UniqueId extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  factory UniqueId() {
+    return UniqueId._(right(const Uuid().v1()));
+  }
+
+  factory UniqueId.fromUniqueString(String uniqueId) {
+    return UniqueId._(
+      right(uniqueId),
+    );
+  }
+
+  const UniqueId._(this.value);
+}
