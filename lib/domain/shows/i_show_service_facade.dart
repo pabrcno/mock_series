296f1222c3aeed0cb926b05c5ef59c8f233794f4
pabrcno <pabrcn@gmail.com>
2021-10-06 23:23:28 -0300

    shows interface implemented

--- lib/domain/shows/i_show_service_facade.dart
+++ lib/domain/shows/i_show_service_facade.dart
@@ -1,6 +1,16 @@
 import 'package:dartz/dartz.dart';
 import 'package:mock_series/domain/shows/show_service_failure.dart';
 
+import 'models/episode.dart';
+import 'models/show.dart';
+
 abstract class IShowServiceFacade {
-  Either<ShowServiceFailure, bool> hello();
+  Either<ShowServiceFailure, List<Show>> getShowsPage({required int page});
+  Either<ShowServiceFailure, List<Show>> getShowsSearch(
+      {required String search});
+  Either<ShowServiceFailure, Show> getShow({required int id});
+  Either<ShowServiceFailure, List<Episode>> getShowSeasons(
+      {required int showId});
+  Either<ShowServiceFailure, List<Episode>> getShowSeasonEpisodes(
+      {required int seasonId});
 }
