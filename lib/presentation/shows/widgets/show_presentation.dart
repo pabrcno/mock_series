da5147ab4b400bbf53879a6e5eb75731b5aebe1e
pabrcno <pabrcn@gmail.com>
2021-10-07 21:33:41 -0300

    mainscreen style defined

--- lib/presentation/shows/widgets/show_presentation.dart
+++ lib/presentation/shows/widgets/show_presentation.dart
@@ -3,41 +3,56 @@ import 'package:mock_series/domain/shows/models/show.dart';
 
 class ShowPresentation extends StatelessWidget {
   final Show show;
+  static const double width = 210;
   const ShowPresentation({Key? key, required this.show}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
-        child: Column(
-      children: [
-        InkWell(
+        child: Container(
+      margin: const EdgeInsets.all(10),
+      child: Column(
+        children: [
+          SizedBox(
+            width: width,
+            child: Text(
+              show.name!,
+              style: const TextStyle(fontSize: 18),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.white, width: 0.5),
+            ),
+            height: 297,
+            width: width,
+            child: show.image != null
+                ? InkWell(
+                    onTap: () {
+                      //TODO: GO TO MOVIE PAGE
+                    },
+                    child: Image.network(
+                      show.image!["medium"]!,
+                      semanticLabel: show.name,
+                    ))
+                : Text(show.name!),
+          ),
+          InkWell(
+            onTap: () {
+              print("like");
+            },
             child: Container(
-          decoration: BoxDecoration(
-              border: Border.all(color: Colors.white, width: 0.5)),
-          height: 210,
-          width: 148,
-          child: show.image != null
-              ? Image.network(
-                  show.image!["medium"]!,
-                  semanticLabel: show.name,
-                )
-              : Text(show.name!),
-        )),
-        Container(
-          width: 150,
-          height: 25,
-          decoration:
-              const BoxDecoration(color: Color.fromRGBO(138, 90, 171, 1)),
-          child: const Center(child: Icon(Icons.favorite, color: Colors.white)),
-        ),
-        const SizedBox(height: 10),
-        Container(
-          width: 74,
-          height: 25,
-          decoration: const BoxDecoration(color: Colors.transparent),
-          child: Text(show.name!),
-        ),
-      ],
+              width: width + 2,
+              height: 50,
+              decoration:
+                  const BoxDecoration(color: Color.fromRGBO(138, 90, 171, 1)),
+              child: const Center(
+                  child: Icon(Icons.favorite, color: Colors.white)),
+            ),
+          ),
+          const SizedBox(height: 10),
+        ],
+      ),
     ));
   }
 }
